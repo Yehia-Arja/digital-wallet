0b64623dd071750b2d523c6a673b80c39a5b5ac1
Yehia Arja <yehia.al.arja12@gmail.com>
2025-03-03 11:28:56 +0200

    fixed referencing user_id

--- wallet-server/connection/models/users.php
+++ wallet-server/connection/models/users.php
@@ -17,15 +17,15 @@ class Users {
         return $sql->get_result()->fetch_assoc();
     }
 
-    public static function getUserById($id) {
+    public static function getUserById($user_id) {
         global $conn;
         $sql = $conn->prepare("SELECT * FROM users WHERE user_id = ?");
-        $sql->bind_param("i", $id);
+        $sql->bind_param("i", $user_id);
         $sql->execute();
         return $sql->get_result()->fetch_assoc();
     }
 
-    public static function updateUser($id, $fullName, $address) {
+    public static function updateUser($user_idid, $fullName, $address) {
         global $conn;
         $sql = $conn->prepare("UPDATE users SET full_name = ?, address = ? WHERE user_id = ?");
         $sql->bind_param("ssi", $fullName, $address, $id);
@@ -38,10 +38,10 @@ class Users {
         $sql->bind_param("si", $new_password, $user_id);
         return $sql->execute();
     }
-    public static function deleteUser($id) {
+    public static function deleteUser($user_id) {
         global $conn;
         $sql = $conn->prepare("DELETE FROM users WHERE user_id = ?");
-        $sql->bind_param("i", $id);
+        $sql->bind_param("i", $user_id);
         return $sql->execute();
     }
 }
